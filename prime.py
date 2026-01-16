8126caf5fef7656a5e3a68e5612e07fca0969364
Varsha-Kurdekar <varshavkurdekar@gmail.com>
2026-01-16 15:17:48 +0530

    save

---
+++ prime.py
@@ -0,0 +1,17 @@
+def get_prime_details():
+    num = int(input("Enter a number:"))
+    if (num % i == 0):
+        print("not_prime:")
+    else:
+        print("prime:")
+        result = {
+            num 
+        }
+        return result
+def display_prime_details(prime):
+    print("number details:")
+    print("number is prime:",prime)
+    print("number is not prime:",not_prime)
+if "__name__" == "__main__":
+    prime = get_prime_details()
+    display_prime_details(prime)
