b139717c638e0a04e893f68577ba7931db63264c
Varsha-Kurdekar <varshavkurdekar@gmail.com>
2026-01-16 16:43:37 +0530

    save

--- pri.py
+++ pri.py
@@ -1,17 +1,17 @@
 def get_prime_details():
     num = int(input("Enter a number: "))
     if num <= 1:
-        return num, not_prime
+        return num, False
     for i in range(2, num):
         if num % i == 0:
-            return num, not_prime
-    return num, prime
+            return num, False
+    return num, True
 def display_prime_details(num, is_prime):
     print("Number details:")
     if is_prime:
-        print("Number is prime:", prime)
+        print("Number is prime:", num)
     else:
-        print("Number is not prime:", not_prime)
+        print("Number is not prime:", num)
 if __name__ == "__main__":
     num, prime = get_prime_details()
     display_prime_details(num, prime)
